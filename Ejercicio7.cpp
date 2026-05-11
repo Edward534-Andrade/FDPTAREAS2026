3267e86b4acba67acaae6401470d9c05161bba11
Edward534-Andrade <00160126@uca.edu.sv>
2026-05-11 12:41:08 -0600

    Ejercicio7

---
+++ Ejercicio7.cpp
@@ -0,0 +1,21 @@
+//Cuenta Regresiva
+//Solicita un número y muestra una cuenta regresiva hasta 0.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int contador ;
+    cout << "Ingrese un numero para iniciar cuenta regresiva: ";
+    cin >> contador ;
+    //el will smith
+      while (contador >= 0 ) {
+        cout << "Contador en while: " << contador << endl;
+        contador--; // Disminuye el contador 
+    }
+
+    return 0;
+
+
+
+
+}
